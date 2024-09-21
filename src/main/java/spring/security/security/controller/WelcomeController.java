00520cbc4ee82d81e85ae777cc93c84229ddbfa2
KICH Yassine <kishyassin@gmail.com>
2024-09-21 13:31:09 +0100

    start point

---
+++ src/main/java/spring/security/security/controller/WelcomeController.java
@@ -0,0 +1,15 @@
+package spring.security.security.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+
+@RestController
+public class WelcomeController {
+    @GetMapping()
+    public String getMethodName() {
+        return "Welcome to spring boot";
+    }
+    
+}
