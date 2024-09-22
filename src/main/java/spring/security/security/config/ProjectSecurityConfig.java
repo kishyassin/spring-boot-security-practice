95a8b8f64c3c1d4324e74390c84d2829995aa943
KICH Yassine <kishyassin@gmail.com>
2024-09-22 14:55:40 +0100

    password hashing and force using strong password

--- src/main/java/spring/security/security/config/ProjectSecurityConfig.java
+++ src/main/java/spring/security/security/config/ProjectSecurityConfig.java
@@ -4,12 +4,17 @@ import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
+import org.springframework.security.authentication.password.CompromisedPasswordChecker;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.password.HaveIBeenPwnedRestApiPasswordChecker;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
@@ -33,7 +38,20 @@ public class ProjectSecurityConfig {
     @Bean
     public UserDetailsService userDetailsService() {
         UserDetails user = User.withUsername("user").password("{noop}password").authorities("read").build();
-        UserDetails admin = User.withUsername("admin").password("{noop}password").authorities("admin").build();
+        UserDetails admin = User.withUsername("admin").password("{bcrypt}$2a$12$oSi1NQBVljERf8YN8c.FCukJ.VZlSIPkZNvY6EUVQhYJ5g1Layk9G").authorities("admin").build();
         return  new InMemoryUserDetailsManager(user, admin);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        // return new BCryptPasswordEncoder();
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder(); // uses the default password encoder, which is at this time; "BCrypt"
+    }
+
+
+    // force using strong passwords instead of easy to guess password
+    @Bean
+    public CompromisedPasswordChecker compromisedPasswordChecker(){
+        return new HaveIBeenPwnedRestApiPasswordChecker();
     }
 }
